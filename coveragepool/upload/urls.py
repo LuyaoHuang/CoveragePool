2d8c2663bc141422f8d5f488202b90a60dbbfb02
Luyao Huang <lhuang@redhat.com>
2016-09-02 15:55:31 +0800

    Add listfile api
    Signed-off-by: Luyao Huang <lhuang@redhat.com>

--- coveragepool/upload/urls.py
+++ coveragepool/upload/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^coveragefile/$', views.coveragefile, name='coveragefile')
+    url(r'^coveragefile/$', views.coveragefile, name='coveragefile'),
+    url(r'^listfile/$', views.listfile, name='listfile'),
 ]
--- coveragepool/upload/views.py
+++ coveragepool/upload/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import CoverageFile
 from dateutil import parser
 import time
 from django.views.decorators.csrf import csrf_exempt
-from .forms import UploadFileForm
+from django.forms.models import model_to_dict
 
 # Create your views here.
 
@@ -27,3 +27,21 @@ def coveragefile(request):
                                      date=date, version=version)
     cf.save()
     return HttpResponse("OK", content_type="text/plain; charset=utf-8")
+
+
+def listfile(request):
+    ret = []
+
+    print request.POST
+    if 'name' in request.POST.keys():
+        objs = CoverageFile.objects.filter(name=request.POST['name'])
+    else:
+        objs = CoverageFile.objects.all()
+
+    for obj in objs:
+        tmp_json = model_to_dict(obj)
+        tmp_json['date'] = tmp_json['date'].strftime("%Y-%m-%d %H:%M:%S")
+        tmp_json['coveragefile'] = tmp_json['coveragefile'].name
+        ret.append(tmp_json)
+
+    return JsonResponse({'data': ret})
