a5f020988067236720e1929039189fba6e81250f
Luyao Huang <lhuang@redhat.com>
2016-09-21 10:23:06 +0800

    Add model to admin
    Signed-off-by: Luyao Huang <lhuang@redhat.com>

--- coveragepool/upload/admin.py
+++ coveragepool/upload/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CoverageFile
 
 # Register your models here.
+class CoverageFileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user_name', 'version', 'date', 'coveragefile')
+    list_filter = ('user_name', 'version')
+    date_hierarchy = 'date'
+
+admin.site.register(CoverageFile, CoverageFileAdmin)
