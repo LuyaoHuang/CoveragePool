e8c03ec23043869c1b5463be11f393774b238d75
Luyao Huang <lhuang@redhat.com>
2016-12-13 22:01:21 +0800

    Add a new model Project to support multiple project
    Signed-off-by: Luyao Huang <lhuang@redhat.com>

--- coveragepool/upload/admin.py
+++ coveragepool/upload/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from .models import CoverageFile
+from .models import CoverageFile, CoverageReport, Project
 
 # Register your models here.
 class CoverageFileAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user_name', 'version', 'date', 'coveragefile')
-    list_filter = ('user_name', 'version')
+    list_display = ('name', 'user_name', 'version',
+                    'date', 'coveragefile', 'project')
+    list_filter = ('user_name', 'version', 'project')
+    search_fields = ('name',)
     date_hierarchy = 'date'
 
 admin.site.register(CoverageFile, CoverageFileAdmin)
+
+class CoverageReportAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project', 'version', 'date',
+                    'tracefile', 'rules', 'url', 'path')
+    list_filter = ('version', 'project')
+    search_fields = ('name',)
+    date_hierarchy = 'date'
+
+admin.site.register(CoverageReport, CoverageReportAdmin)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'base_dir', 'base_url',
+                    'tag_fmt', 'git_repo', 'gs_key', 'gs_json_file')
+
+admin.site.register(Project, ProjectAdmin)
--- coveragepool/upload/models.py
+++ coveragepool/upload/models.py
@@ -6,6 +6,7 @@ from django.db.models.signals import post_delete
 from django.core.files import File
 import random
 import shutil
+import datetime
 
 def content_file_name(instance, filename):
     hash = random.getrandbits(128)
@@ -13,17 +14,32 @@ def content_file_name(instance, filename):
     return 'coveragefile/%s' % file_name
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    base_dir = models.CharField(max_length=100, null=True)
+    base_url = models.CharField(max_length=100, null=True)
+    tag_fmt = models.CharField(max_length=100, null=True)
+    git_repo = models.CharField(max_length=255, null=True)
+    # Google sheet db
+    gs_key = models.CharField(max_length=255, null=True)
+    gs_json_file = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
 class CoverageFile(models.Model):
+    project = models.ForeignKey(Project, null=True)
     name = models.CharField(max_length=100)
     user_name = models.CharField(max_length=100)
     version = models.CharField(max_length=100)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=datetime.datetime.now)
     coveragefile = models.FileField(upload_to=content_file_name)
 
 class CoverageReport(models.Model):
+    project = models.ForeignKey(Project, null=True)
     name = models.CharField(max_length=100)
     version = models.CharField(max_length=100)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=datetime.datetime.now)
     path = models.CharField(max_length=100, blank=True, null=True)
     url = models.CharField(max_length=100, blank=True, null=True)
     tracefile = models.FileField(null=True, upload_to=content_file_name)
